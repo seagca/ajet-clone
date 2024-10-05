301e8c58aaca6c94c4d76bee8141f48621914c2f
IIIIIIIIII <salihemre29072004@gmail.com>
2024-10-05 05:56:21 +0300

    Tried to fix routing for now Need to change hrefs for them to work because root is not set properly

--- src/router/router.js
+++ src/router/router.js
@@ -9,21 +9,22 @@ export const router = createRouter({
     history: createWebHistory(),
     routes: [
       {
-    path: '/ajet-clone',
-    redirect: '/ajet-clone/en' // The default route to your Home component
-      },
-      {
     path: '/',
-    redirect: '/ajet-clone/en' // The default route to your Home component
+    redirect: '/ajet-clone' // The default route to your Home component
       },
       {
     path: '/en',
-    redirect: '/ajet-clone/en' // The default route to your Home component
+    redirect: '/ajet-clone' // The default route to your Home component
       },
   {
-    path: '/ajet-clone/en',
+    path: '/ajet-clone',
     name: 'AppHome',
     component: AppHome // The default route to your Home component
+      },
+  {
+    path: '/ajet-clone:catchAll(.*)', // Catch-all route for undefined paths
+    name: 'NotFound',
+    component: NotFound // This points to the custom 404 page
       },
   {
     path: '/:catchAll(.*)', // Catch-all route for undefined paths
